d6560a01d0719690470076f173ee27fc85d4f011
Pranav Vashi <neobuddy89@gmail.com>
2024-06-29 23:26:20 +0530

    SystemUIGoogle: Update ThemeOverlayController for A14 QPR3
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: ifloraxd <ifloraxd@gmail.com>

--- SystemUIGoogle/src/com/google/android/systemui/theme/ThemeOverlayControllerGoogle.kt
+++ SystemUIGoogle/src/com/google/android/systemui/theme/ThemeOverlayControllerGoogle.kt
@@ -17,6 +17,7 @@
 
 package com.google.android.systemui.theme
 
+import android.app.ActivityManager
 import android.app.UiModeManager
 import android.app.WallpaperManager
 import android.content.Context
@@ -66,6 +67,7 @@ class ThemeOverlayControllerGoogle @Inject constructor(
     javaAdapter: JavaAdapter,
     keyguardTransitionInteractor: KeyguardTransitionInteractor,
     uiModeManager: UiModeManager,
+    activityManager: ActivityManager,
     configurationController: ConfigurationController,
     tunerService: TunerService,
     @param:Main private val mainResources: Resources,
@@ -91,7 +93,8 @@ class ThemeOverlayControllerGoogle @Inject constructor(
     keyguardTransitionInteractor,
     uiModeManager,
     configurationController,
-    tunerService    
+    tunerService,
+    activityManager
 ) {
     init {
         configurationController.addCallback(object :
